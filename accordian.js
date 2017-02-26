9f7acbc16fc13d33732caaa9d3bdd6a40db86041
Jonathan Christian <BeYourExcitement@gmail.com>
2017-02-26 06:57:49 -0500

    Add isAccordianSectionActive and accordianSectionId

--- accordian.js
+++ accordian.js
@@ -3,23 +3,34 @@ window.addEventListener("load", function() {
   
   // ********** encapsulate event in one variable for production ******************
   
-  
-  var accordianSectionHeaders = document.getElementsByTagName("h3")
+  var isAccordianSectionActive = {
+    section1: false,
+    section2: false,
+    section3: false
+  }
 
   var accordianClickEventHandler = function(element) {
     element.addEventListener ("click", function() {
       
       var accordianSectionTag = element.nextElementSibling
+      var accordianSectionId = accordianSectionTag.id
+
       // if accordianSectionHeaders[0] has class "accordian-section-active" set it without 'active'
-      // if accordianSectionTag.className === "accordian-section-active"
+      if (accordianSectionTag.className === "accordian-section-active")
       
       // changes class to 'active'
       accordianSectionTag.className = "accordian-section-active"
     })
   }
 
+
+  var accordianSectionHeaders = document.getElementsByTagName("h3")
+
   for (var i = accordianSectionHeaders.length - 1; i >= 0; i--) {
     accordianClickEventHandler (accordianSectionHeaders[i])
   }
+
+
+
 
 })
