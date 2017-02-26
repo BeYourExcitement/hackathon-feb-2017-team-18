c72ccad6a649561671265fceaabe877e172bf221
Jonathan Christian <BeYourExcitement@gmail.com>
2017-02-26 07:49:05 -0500

    Basic accordian works

--- accordian.js
+++ accordian.js
@@ -3,34 +3,25 @@ window.addEventListener("load", function() {
   
   // ********** encapsulate event in one variable for production ******************
   
-  var isAccordianSectionActive = {
-    section1: false,
-    section2: false,
-    section3: false
-  }
+  var currentAccordianActiveSection = ""
 
   var accordianClickEventHandler = function(element) {
     element.addEventListener ("click", function() {
       
       var accordianSectionTag = element.nextElementSibling
-      var accordianSectionId = accordianSectionTag.id
-
-      // if accordianSectionHeaders[0] has class "accordian-section-active" set it without 'active'
-      if (accordianSectionTag.className === "accordian-section-active")
       
-      // changes class to 'active'
+      if (currentAccordianActiveSection.length !== 0) {
+        currentAccordianActiveSection.className = "accordian-section"
+      }  
+      // changes `section` tag class to 'accordian-section-active'
       accordianSectionTag.className = "accordian-section-active"
+      currentAccordianActiveSection = accordianSectionTag
     })
   }
-
 
   var accordianSectionHeaders = document.getElementsByTagName("h3")
 
   for (var i = accordianSectionHeaders.length - 1; i >= 0; i--) {
     accordianClickEventHandler (accordianSectionHeaders[i])
   }
-
-
-
-
 })
