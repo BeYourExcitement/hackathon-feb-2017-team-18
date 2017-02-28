067a0da3f81908547b117ad7a54ea4e6a5920932
Jonathan Christian <BeYourExcitement@gmail.com>
2017-02-28 06:17:08 -0500

    Refactor accordian javascript

--- docs/js/components.js
+++ docs/js/components.js
@@ -4,42 +4,49 @@ window.addEventListener("load", function() {
 // -------------------------------------------------
 // Accordion by Jonathan Christian @BeYourExcitement
 // -------------------------------------------------
+// Refactor and fully utilize those objects and key/value pairs
   
-  // refactoring
-  // ------------------------
-  // var accordionElement = document.getElementsByClassName ("accordion")
-  // alert(accordionElement)
-  // var accordionH3InnerHtml = accordionElement.getElementsByTagName ("h3")
-  // alert(accordionH3InnerHtml)
+  // keys = h3 innerHTML / values = h3 element
+  var accordionH3Elements = {}
+  // keys = section id / values = section element
+  var accordionSectionElements = {}
 
-  var currentActiveAccordionSection = ""
+  var currentActiveAccordionH3InnerHtml = ""
+  var currentActiveAccordionSectionId = ""
 
-  var accordionClickEvent = function (element) {
-    element.addEventListener ("click", function() {
-      
-      var accordionSectionTag = element.nextElementSibling
+  var accordionClickEvent = function (h3Element) {
+    var accordionSectionElement = h3Element.nextElementSibling
+    
+    h3Element.addEventListener ("click", function() {
+      if (currentActiveAccordionH3InnerHtml.length !== 0) {
+        var currentActiveH3Element = accordionH3Elements[currentActiveAccordionH3InnerHtml]
+        var currentActiveSectionElement = accordionSectionElements[currentActiveAccordionSectionId]
 
-      if (currentActiveAccordionSection.length === 0) {
-        // changes the tag `section` class to 'accordion-section-active'
-        accordionSectionTag.className = "accordion-section-active"
-        currentActiveAccordionSection = accordionSectionTag
-       } 
-
-      if (currentActiveAccordionSection.length !== 0) {
-        currentActiveAccordionSection.className = "accordion-section"
-        accordionSectionTag.className = "accordion-section-active"
-        currentActiveAccordionSection = accordionSectionTag
+        // deactivates accordion h3 and section
+        currentActiveH3Element.className = "section-header"
+        currentActiveSectionElement.className = "accordion-section"
       }
+
+      // activates H3 and Section elements in Accordion
+      h3Element.className = "section-header-active"
+      accordionSectionElement.className = "accordion-section-active"
+      currentActiveAccordionH3InnerHtml = h3Element.innerHTML
+      currentActiveAccordionSectionId = accordionSectionElement.id
     })
   }
-  // Collects h3 tags in document into array
-  var accordionSectionHeaders = document.getElementsByTagName("h3")
 
-  // Adds click event to h3 tags within accordion section
-  for (var i = accordionSectionHeaders.length - 1; i >= 0; i--) {
-    // if (accordionSectionHeaders[i].className === "section-header") {then execute code below}
-    accordionClickEvent (accordionSectionHeaders[i])
+  // Collects h3 tags in document into array
+  var accordionH3Collection = document.getElementsByClassName ("section-header")
+
+  // Adds click event to h3 tags and create key/value pair of h3.innerHTML/section.id
+  for (var i = accordionH3Collection.length - 1; i >= 0; i--) {
+    // set key/value pairs for accordion h3 Elements and accordion section Elements 
+    accordionH3Elements[accordionH3Collection[i].innerHTML] = accordionH3Collection[i]
+    accordionSectionElements[accordionH3Collection[i].nextElementSibling.id] = accordionH3Collection[i].nextElementSibling
+    // Add click event to h3 elements
+    accordionClickEvent (accordionH3Collection[i])
   }
+
 
 // -----------------------------------------------------------------------
 // Carousel by Jonathan Christian @BeYourExcitement
@@ -62,10 +69,10 @@ window.addEventListener("load", function() {
     thumbnailElement.addEventListener ("click", function() {
       var deactivatecurrentActiveCenterpieceImageElement = document.getElementById (imageIds[currentlyClickedThumbnailId])
 
-      // activates the clicked thumbnail as the centerpiece
+      // Deactivates the clicked thumbnail as the centerpiece
       deactivatecurrentActiveCenterpieceImageElement.className = "centerpiece-image"
       
-      // Locates and activates a centerpiece image
+      // Activates a centerpiece image
       centerpieceImageElement.className = "centerpiece-image-active"
       // changes currentlyClickedThumbnailId to the currently clicked thumbnail ID
       currentlyClickedThumbnailId = thumbnailElement.id
